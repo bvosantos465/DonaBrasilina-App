127b79ed64bdb2ee90bf22398dcc43872e984806
bvosantos465 <32101744+bvosantos465@users.noreply.github.com>
2018-10-21 13:34:14 -0300

    Delete Response.kt

--- Response.kt
+++
@@ -1,6 +0,0 @@
-package com.example.byah_.donabrasilina
-
-data class Response (val status:String, val msg:String) {
-    // verificar se o chamada foi executada com sucesso
-    fun isOK() = "OK".equals(status, ignoreCase = true)
-}
